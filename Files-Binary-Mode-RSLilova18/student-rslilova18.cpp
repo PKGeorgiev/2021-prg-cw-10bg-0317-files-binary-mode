ce498693740931f26f99391a6ccf6185d24862c5
RSLilova18 <RSLilova18@codingburgas.bg>
2021-03-17 10:15:54 +0200

    Implement for loop that writes 6 numbers in binary mode

---
+++ Files-Binary-Mode-RSLilova18/student-rslilova18.cpp
@@ -0,0 +1,10 @@
+#include "student-rslilova18.h"
+using namespace std;
+
+char* STUDENT::getFullName()
+{
+	char* result = new char[sizeof(firstName) + sizeof(lastName) + sizeof(char)];
+	return nullptr;
+}
+
+
