ae463b876c44a6c7b1fad4573c8beaba8afb2c21
SATodorov18 <59693603+SATodorov18@users.noreply.github.com>
2021-03-17 10:16:24 +0200

    implement for fool that writes 6 numbers in binary mode.

--- files-binary-mode-pkgeorgiev/files-binary-mode-pkgeorgiev.cpp
+++ files-binary-mode-pkgeorgiev/files-binary-mode-pkgeorgiev.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include "student-satodorov18.h"
 
 using namespace std;
 
 int main()
 {
-    
+    fstream file;
+    file.open("satodorov18.bin", ios::out or ios::binary or ios::trunc);
+    if (file.is_open())
+    {
+        int a = 65;
+        file.write((const char*)&a, sizeof(a));
+
+        file.close();
+    }
 }
---
+++ files-binary-mode-pkgeorgiev/student-satodorov18.cpp
@@ -0,0 +1,7 @@
+#include "student-satodorov18.h"
+
+char* STUDENT::getFullName()
+{
+    char* result = new char{ sizeof(firstName) + sizeof(lastName) + sizeof(char) };
+    return nullptr;
+}
