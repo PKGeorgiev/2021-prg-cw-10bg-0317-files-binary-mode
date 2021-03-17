0338d9c7b233f5744f2e74d3953bc47cb3a3b1fb
SDDimitrov18 <sddimitrov18@codingburgas.bg>
2021-03-17 10:19:53 +0200

    fixed something

---
+++ files-binary-mode-pkgeorgiev/adsf.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct STUDENT {
+    char firstName[20];
+    char lastName[20];
+    int age;
+    int grade;
+
+    char* getFullName();
+};
--- files-binary-mode-pkgeorgiev/files-binary-mode-pkgeorgiev.cpp
+++ files-binary-mode-pkgeorgiev/files-binary-mode-pkgeorgiev.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <fstream>
+#include "adsf.h"
 using namespace std;
 
 int main()
 {
-    
+    fstream file;
+    file.open("pbborisov18.bin", ios::out | ios::binary | ios::trunc);
+
+    if (file.is_open()) {
+        int a = 65;
+
+
+        for (size_t i = 0; i < 6; i++) {
+            file.write((const char*)&i, sizeof(i));
+        }
+        file.close();
+    }
 }
