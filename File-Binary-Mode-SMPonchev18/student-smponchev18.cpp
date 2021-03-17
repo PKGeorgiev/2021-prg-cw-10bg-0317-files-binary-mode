cbaf104d3788e3933bb58d24efbcc7febe63b904
SMPonchev18 <SMPonchev18@codingburgas.bg>
2021-03-17 10:16:08 +0200

    Implement for loop that writes 6 numbers in binary mode

---
+++ File-Binary-Mode-SMPonchev18/student-smponchev18.cpp
@@ -0,0 +1,8 @@
+#include "student-smponchev18.h"
+
+char* STUDENT::getFullName()
+{
+    char* result = new char [sizeof(firstName) + sizeof(lastName) + sizeof(lastName)];
+    // TODO: copy fullName + ' ' + lastName
+    return nullptr;
+}
